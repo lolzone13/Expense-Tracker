18e1d80ef5d9e2c1c0770f9422a768ad805dbbfa
lolzone13 <mohitscience123@gmail.com>
2021-12-12 01:37:40 +0530

    Created basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css';
+import Header  from './Components/Header';
+import Balance from './Components/Balance';
+import IncomeExpenses from './Components/IncomeExpenses';
+import TransactionList from './Components/TransactionList';
+import AddTransaction from './Components/AddTransaction';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
