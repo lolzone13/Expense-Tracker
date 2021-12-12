c8c7dd8e926897674dc0eff73d703c387a7a9117
lolzone13 <mohitscience123@gmail.com>
2021-12-12 19:41:54 +0530

    cleanup

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,7 +1,25 @@
 // @desc get all transactions
-// @route /api/v1/transactions/get
+// @route GET /api/v1/transactions
 // @access public (no auth)
 
 exports.getTransactions = (req, res, next) => {
     res.send('GET transactions')
+}
+
+// @desc add transaction
+// @route POST /api/v1/transactions
+// @access public (no auth)
+
+exports.addTransactions = (req, res, next) => {
+    res.send('ADD transactions')
+}
+
+// @desc delete transaction
+// @route DELETE /api/v1/transactions/:id
+// @access public (no auth)
+
+exports.deleteTransactions = (req, res, next) => {
+    //res.write(req);
+    res.send('DELETE transactions');
+    //res.send();
 }
