16138a8687ee4d3dc13d4d5ecd34d4da4c780f54
lolzone13 <mohitscience123@gmail.com>
2021-12-12 23:54:42 +0530

    gitignore, added models

---
+++ models/Transaction.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+
+const TransactionSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        trim: true,
+        required: [true, 'Please add text']
+    }
+})
--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
